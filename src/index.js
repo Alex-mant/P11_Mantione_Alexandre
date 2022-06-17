2081d297c94bc823631a2d345facd1c40669d5a8
Alex Mant <al.mantione@gmail.com>
2022-06-17 15:05:57 +0200

    ajout ScrollToTop

--- src/index.js
+++ src/index.js
@@ -7,17 +7,20 @@ import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Home from './components/2_Pages/Home/Home';
 import About from './components/2_Pages/About/About';
 import Error404 from './components/2_Pages/Error404/Error404';
+import ScrollToTop from './assets/utils/ScrollToTop';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Router>
         < Header />
-        <Routes>
-          < Route exact path='/kasa' element= {<Home />} />  
-          < Route path='/About/' element= {<About />} />
-          < Route path="*" element= {<Error404 />} />  
-        </Routes>
+        <ScrollToTop>
+          <Routes>            
+            < Route exact path='/kasa' element= {<Home />} />  
+            < Route path='/About/' element= {<About />} />
+            < Route path="*" element= {<Error404 />} />  
+          </Routes>
+        </ScrollToTop>
         < Footer />
     </Router>
   </React.StrictMode>
