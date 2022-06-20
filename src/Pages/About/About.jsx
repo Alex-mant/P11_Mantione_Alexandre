a11db46f0ab1ac58e9513740ac657f0d49ac3553
Alex Mant <al.mantione@gmail.com>
2022-06-20 16:56:54 +0200

    fin de la page about

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -1,13 +1,15 @@
 import React from "react"
 import AboutBanner from "../../components/AboutBanner/AboutBanner";
 import Main from "../../components/AboutMain/AboutMain";
-
+import { data } from "../../data/data";
 
 const About = () => {
+  const myData = data;
+
   return (
     <React.Fragment>
       <AboutBanner />
-      <Main />
+      <Main myData={myData} />
     </React.Fragment>
   )
 }
