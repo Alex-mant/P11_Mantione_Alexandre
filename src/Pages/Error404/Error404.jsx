3f621aea32fd464f1161f4955a44a92cb1638a11
Alex Mant <al.mantione@gmail.com>
2022-06-24 12:45:15 +0200

    maj link error404 to homePage

--- src/Pages/Error404/Error404.jsx
+++ src/Pages/Error404/Error404.jsx
@@ -13,7 +13,7 @@ const Error404 = () => {
       <h3 className="Error-title Error-text-format">404</h3>
       <p className="Error-message Error-text-format">Oups! La page que vous demandez n'existe pas.</p>
       <nav className="back-to-home">
-        <Link className="Error-link Error-text-format" to="/kasa">Retourner à la page d'accueil</Link>
+        <Link className="Error-link Error-text-format" to="/kasa/home">Retourner à la page d'accueil</Link>
       </nav>
     </main>
   )
