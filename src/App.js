ebdd3f04a8c69d7e7e5dc8a22703e5518c3d7419
Alex Mant <al.mantione@gmail.com>
2022-06-24 17:07:45 +0200

    axios mise en place

--- src/App.js
+++ src/App.js
@@ -6,16 +6,16 @@ import Home from './Pages/Home/Home';
 import Error404 from './Pages/Error404/Error404';
 import About from './Pages/About/About';
 import Footer from './components/Footer/Footer';
-import axios from 'axios';
-import { useEffect, useState } from 'react';
+// import axios from 'axios';
+// import { useEffect, useState } from 'react';
 
 const App = () => {
 
-  const [data, setData] = useState([]);
+  // const [data, setData] = useState([]);
 
-  useEffect(() => {
-    axios.get("https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/Front-End+V2/P9+React+1/logements.json").then((res) => setData(res.data))
-  }, []);
+  // useEffect(() => {
+  //   axios.get("https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/Front-End+V2/P9+React+1/logements.json").then((res) => setData(res.data))
+  // }, []);
 
   return (
     <Router>
